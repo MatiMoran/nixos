b703fb60faad59599c07916dd9aa9098ab222f68
Matias Moran <matiec52000@hotmail.com>
2026-07-31 19:33:22 -0300

    symlink full .mame folder to dotfiles and version mame configs

--- modules/home/mame.nix
+++ modules/home/mame.nix
@@ -10,17 +10,8 @@
     {
       home.packages = [ pkgs.mame ];
 
-      home.file.".mame/mame-dl.py" = {
-        source = ../../dotfiles/mame/mame-dl.py;
-        executable = true;
-      };
-
-      home.file.".mame/games.txt" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${repoDir}/dotfiles/mame/games.txt";
-      };
-
-      home.file.".mame/mame.ini" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${repoDir}/dotfiles/mame/mame.ini";
+      home.file.".mame" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${repoDir}/dotfiles/mame";
       };
     })
   ];
